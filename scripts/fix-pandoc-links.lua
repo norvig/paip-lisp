7598e3c39f151c79456a6534eb24ebf91a22ccbb
James Young <pronoiac@gmail.com>
2024-11-16 15:09:20 -0600

    For Pandoc and epub generation, fix links between chapters
    Between chapters, without anchors, change the link to e.g. "#chapter15_top".
    I'll add those anchors in another commit.

--- scripts/fix-pandoc-links.lua
+++ scripts/fix-pandoc-links.lua
@@ -1,7 +1,14 @@
--- fix links between chapters, to anchors
--- via https://stackoverflow.com/questions/48169995/pandoc-how-to-link-to-a-section-in-another-markdown-file
+-- fix pandoc links
+-- called from make-epub.sh
 
 function Link (link)
+  -- fix links between chapters, to anchors
+  -- e.g. turn chapter15.md#p514 into #p514
+  -- via https://stackoverflow.com/questions/48169995/pandoc-how-to-link-to-a-section-in-another-markdown-file
   link.target = link.target:gsub('.+%.md%#(.+)', '#%1')
+
+  -- e.g. turn chapter15.md into #chapter15_top
+  link.target = link.target:gsub('(.+)%.md$', '#%1_top')
+
   return link
 end
